f6c9f59347d23fccea8721353ee2217177b099de
Cub <qubard@gmail.com>
2022-02-05 18:51:40 -0800

    elements test

--- judge/codegen/input_sanitizer_test.go
+++ judge/codegen/input_sanitizer_test.go
@@ -8,11 +8,14 @@ import (
 
 func TestInputRegex(t *testing.T) {
 	assert.NotNil(t, isNumberList("[0,1,2]"))
+	assert.Equal(t, "0,1,2", isNumberList("[0,1,2]").Elements)
 	assert.NotNil(t, isNumberList("[0, 1,2]"))
 	assert.NotNil(t, isNumberList("[ 0, 1,2]"))
 	assert.NotNil(t, isNumberList("[ 0, 1, 2]"))
 	assert.NotNil(t, isNumberList("[ 0, 1, 2 ]"))
 	assert.NotNil(t, isNumberList("[ 0, 1.5, 2 ]"))
+	assert.Equal(t, "0", isNumberList("[0]").Elements)
+	assert.Equal(t, "1.5", isNumberList("[1.5]").Elements)
 	assert.NotNil(t, isNumberList("[ 1.5, 1.5, 2.3 ]"))
 	assert.Nil(t, isNumberList("[ 0, 1, 2, ]"))
 }
