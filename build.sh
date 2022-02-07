7bc0fa130f9382af7aa170940c8282c08c21a94d
Cub <qubard@gmail.com>
2022-02-07 00:31:59 -0800

    cgo

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-GOARCH=amd64 GOOS=linux go build compiler_lambda.go 
-GOARCH=amd64 GOOS=linux go build judge_lambda.go && docker build -f Dockerfile .
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build compiler_lambda.go 
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build judge_lambda.go && docker build -f Dockerfile .
